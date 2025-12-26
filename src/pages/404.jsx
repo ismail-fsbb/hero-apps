f32004994efdb9d41e0354df56fccdb07c10f760
Ismail Hossain <82139845+ismail-fsbb@users.noreply.github.com>
2025-12-27 02:52:25 +0600

    Solve build issues

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 export default function NotFound() {
